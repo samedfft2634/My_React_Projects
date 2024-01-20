6f06baa9019d4c4bec838a37fee0f27cb6c910c8
Samed <100915606+samedfft2634@users.noreply.github.com>
2024-01-20 23:09:39 +0300

    Delete src directory

--- src/router/PrivateRouter.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router-dom"
-
-const PrivateRouter = () => {
-  const {token} = useSelector(state=> state.auth)
-  return token ? <Outlet /> : <Navigate to="/"  /> 
-}
-
-export default PrivateRouter
